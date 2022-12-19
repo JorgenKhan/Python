b8a8c063ab44782300cb00609dfb0d87d6c5a256
unknown <adil.erzhan@gmail.com>
2022-12-19 16:57:56 +0300

    HW1_Task1

---
+++ Sem1/Homeworks/Task1.py
@@ -0,0 +1,9 @@
+## Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+weekday = input('Input number of weekday: ')
+if weekday == '6':
+    print('It is weekend!!')
+elif weekday == '7':
+    print('It is weekend!!')
+else:
+    print('It is wrong day to relax')
